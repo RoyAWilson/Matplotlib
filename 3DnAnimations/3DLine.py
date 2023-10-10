5cd33b87d13d54dad58ade75cf7048df5df3557c
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-11 00:19:02 +0100

    3D plot values of x/y and cosine x+y

--- 3DnAnimations/3DLine.py
+++ 3DnAnimations/3DLine.py
@@ -7,16 +7,16 @@ from matplotlib import style
 ax = plt.axes(projection='3d')
 
 x = np.arange(0, 50, 0.1)
-y = np.sin(x)
-z = np.cos(x)
+y = np.arange(0, 50, 0.1)
+z = np.cos(x + y)
 
 ax.plot(x, y, z)
 ax.set_xlabel('X Value')
-ax.set_ylabel('Sin of X')
-ax.set_zlabel('Cosine of X')
+ax.set_ylabel('Y Value')
+ax.set_zlabel('Cosine of X + Y')
 ax.set_title('3D Line Plot')
 plt.show()
 
 # POlots in 3d and can rotate the plot by dragging
 
-plt.savefig('../Figures/figure13_1_HQ.png', dpi=300)
+plt.savefig('../Figures/figure13_2_HQ.png', dpi=300)
