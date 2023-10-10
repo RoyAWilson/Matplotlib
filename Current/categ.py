c8e5d56007940329120fd830760125ade9779fba
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 20:40:08 +0100

    Add some refinements to the bar chart

---
+++ Current/categ.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# categorical data in bar chart
+
+x = ['C++', 'C#', 'Python', 'Java', 'Go']
+y = [20, 50, 140, 1, 45]
+plt.bar(x, y, color='Orange', align='edge', width=0.95, edgecolor='Blue', lw=3)
+# Not so sure that I like the thicker line - obliterates the lowest value's bar
+plt.show()
+plt.savefig('../Figures/figure3_2.png')
