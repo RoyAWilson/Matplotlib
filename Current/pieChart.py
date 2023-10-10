ecd02d087d71e8820b16e0e2e150b76967b4ac65
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 21:27:16 +0100

    Pie chart with explode etc.

--- Current/pieChart.py
+++ Current/pieChart.py
@@ -8,8 +8,10 @@ langs = ['Python', 'C++', 'C#', 'Java', 'Go']
 votes = [50, 24, 14, 6, 17]
 
 # Plot it
-plt.pie(votes, labels=langs)
+explodes = [0, 0.1, 0, 0.2, 0]
+plt.pie(votes, labels=langs, explode=explodes,
+        autopct='%.2f%%', pctdistance=0.75, startangle=90)
 plt.show()
 
 # Save it
-plt.savefig('../Figures/figure5_1.png')
+plt.savefig('../Figures/figure5_2.png')
