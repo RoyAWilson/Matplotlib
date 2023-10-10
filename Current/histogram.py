fd79d166f0c6a551d36490350e1f7c6ab23c2983
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 21:02:56 +0100

    Change binning method to list of bins

--- Current/histogram.py
+++ Current/histogram.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 # Use numpy to produce a normal distribution of number
 
 # Argument 1 mean value, arg 2 standard deviation, arg 3 items
-ages = np.random.normal(20, 1.5, 1000)
+ages = np.random.normal(20, 2.5, 1000)
 
 # Plot and show histogram
-plt.hist(ages, bins=20, color='green', )
+# plt.hist(ages, bins=20, color='green', )
+
+# Another way to change bins:
+plt.hist(ages, bins=[2, 4, 6, 8, 10, 12, 14, 16,
+         18, 20, 22, 24, 26, 28, 30], color='green')
 plt.show()
 
 # save the figure
-plt.savefig('../Figures/figure4_2.png')
+plt.savefig('../Figures/figure4_3.png')
