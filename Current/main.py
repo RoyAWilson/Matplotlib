f1f2ab2d4b58aea63a241d15ee5cee618cbf1d59
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 20:07:47 +0100

    Change number of markers and add transparency. Add code to save the figure to figures directory

--- Current/main.py
+++ Current/main.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 
 # Use numpy to produce some example data
 
-x_data = np.random.random(50) * 100
-y_data = np.random.random(50) * 100
+x_data = np.random.random(1000) * 100
+y_data = np.random.random(1000) * 100
 
 # Scatter Plots:
 
-plt.scatter(x=x_data, y=y_data, c='Green', marker='*', s=150)
+z_plot = plt.scatter(x=x_data, y=y_data, c='Green',
+                     marker='*', s=150, alpha=0.3)
 plt.show()
+plt.savefig('../Figures/figure1_2Try.png')
