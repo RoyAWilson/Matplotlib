a56c54f06d8c96cd9c903c1228cda53202f49e94
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 20:20:50 +0100

    Basic Line Chart with year and weight data

---
+++ Current/linechart.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# produce list of years and weights
+
+years = [2006 + x for x in range(16)]
+# print(years)
+weight = [80, 83, 84, 85, 86, 82, 81, 79,
+          83, 80, 82, 82, 83, 81, 80, 79]
+
+# Default plt function plots line chart so no need to call line
+
+plt.plot(years, weight)
+plt.show()
+plt.savefig('../Figures/figure2_1.png')
