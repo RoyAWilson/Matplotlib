3db1055f7f73afcc949a9571598dcb6ba88c501f
Roy Wilson <royawilson@hotmail.co.uk>
2023-10-10 21:34:30 +0100

    Basic box plot only one box

---
+++ Current/boxplot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Box Plot
+
+# Generate some data for plotting
+# arg 1 = mean. arg 2 = standard distribution, arg 3 = number of points
+heights = np.random.normal(172, 8, 300)
+
+# Plot it
+plt.boxplot(heights)
+plt.show()
+
+# and save the fig
+plt.savefig('../Figures/figure6_1.png')
